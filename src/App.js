8b738afd58baf5c3296fbcca313866e733223efd
Benoit Werquin <benoit.werquin@insee.fr>
2021-02-05 17:26:58 +0100

    add first page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Link from '@material-ui/core/Link';
+import SecondaryTitle from './secondaryTitle';
+
 
 function App() {
   return (
-    <Button variant="contained" color="primary">
-      Hello World
-    </Button>
+    <Container maxWidth="sm">
+    <Box my={4}>
+      <Typography variant="h4" align="center" component="h1" gutterBottom>
+        Knowledge
+      </Typography>
+      <SecondaryTitle />
+      <Copyright />
+    </Box>
+  </Container>
+  );
+}
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Powered by '}
+      <Link color="inherit" href="https://github.com/InseeFr/Lunatic">
+        Lunatic
+      </Link>
+      {'.'}
+    </Typography>
   );
 }
 
