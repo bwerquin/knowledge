8af98c1337d505d5ebcb07d0fc79b663172ba798
Benoit Werquin <benoit.werquin@insee.fr>
2021-02-05 17:44:50 +0100

    add footer

--- src/App.js
+++ src/App.js
@@ -3,18 +3,19 @@ import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import Link from '@material-ui/core/Link';
-import SecondaryTitle from './secondaryTitle';
+import SecondaryTitle from './SecondaryTitle';
+import Footer from './Footer';
 
 
 function App() {
   return (
     <Container maxWidth="sm">
     <Box my={4}>
-      <Typography variant="h4" align="center" component="h1" gutterBottom>
+      <Typography variant="h2" align="center" component="h1" gutterBottom>
         Knowledge
       </Typography>
       <SecondaryTitle />
-      <Copyright />
+      <Footer><Copyright /></Footer>
     </Box>
   </Container>
   );
@@ -23,7 +24,7 @@ function App() {
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Powered by '}
+      {'PWA Questionnaire Powered by '}
       <Link color="inherit" href="https://github.com/InseeFr/Lunatic">
         Lunatic
       </Link>
